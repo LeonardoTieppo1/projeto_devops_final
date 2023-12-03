009cc05c1d62eb435d508fbe7b4b24cf065da7ca
LeonardoTieppo1 <Leonardo.tieppo2@hotmail.com>
2023-12-03 01:54:55 -0300

    projeto_devops_final

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/api/hello',methods=['GET'])
 def hello():
     return jsonify({'message': 'Olá, mundo!'})
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
