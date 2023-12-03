3aec23c4993b5a4f4a2608c1092f391963614264
LeonardoTieppo1 <Leonardo.tieppo2@hotmail.com>
2023-12-03 00:54:02 -0300

    projeto_devops_final

--- app.py
+++ app.py
@@ -3,20 +3,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Bem-vindo à API Flask para a VM do Azure!'
-
-@app.route('/conectar_vm')
-def conectar_vm():
-    # Lógica para se conectar à VM do Azure
-    # Substitua este trecho de código com sua lógica real de conexão à VM
-
-    # Exemplo fictício
-    resposta = {
-        'mensagem': 'Hello World!'
-    }
-
-    return jsonify(resposta)
+def hello_world():
+    return jsonify({'message': 'Olá, mundo!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
